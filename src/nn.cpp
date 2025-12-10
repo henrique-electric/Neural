cf226f98b25998b715c677872b945a6782245db7
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-12-10 17:45:26 -0300

    Update neural network init
    Add some comments on the code and initialize the output layer

--- src/nn.cpp
+++ src/nn.cpp
@@ -4,7 +4,7 @@ void NN::fillMatrix(Eigen::MatrixXd &matrix, int rows, int columns) {
     matrix.resize(rows, columns);
     for (int i=0; i < rows; i++) {
         for (int j=0; j < columns; j++) {
-            matrix(i, j) = WInit::xavierInit(this->inputs.size(), this->output.size());
+            matrix(i, j) = WInit::xavierInit(this->inputs.size(), this->output.neurons.size());
         }
     }
 }
@@ -16,16 +16,22 @@ static void fillWeigths(Eigen::VectorXd &weights, int inputSize, int outputSize)
 }
 
 NN::NN(int layers, int neuronsPerLayer, int inputSize, int outputSize) {
-    
+
+    // ========= Initialize the input vector =============
     this->inputs = Eigen::VectorXd(inputSize);
-    this->inputs.setRandom();
+    this->inputs.setRandom(); // Use random inputs just for now
+    // ====================================================
+
     
-    this->output = Eigen::VectorX<Layer>(outputSize);
-    
+    // ======== Initialize the layer vector and get references of the first Hidden layer and the output layer =======
     this->layers = Eigen::VectorX<Layer>(layers);
+    Layer &outputLayer = this->output;
+    Layer &firstLayer = this->layers(0);
+    //================================================================================================================
+    
     
     //==============  Setup the first hidden layer shape ================
-    this->layers(0).input = this->inputs;
+    firstLayer.input = this->inputs;
     this->layers(0).output = Eigen::VectorXd(neuronsPerLayer);
     this->layers(0).neurons = Eigen::VectorX<Neuron>(neuronsPerLayer);
 
@@ -54,6 +60,7 @@ NN::NN(int layers, int neuronsPerLayer, int inputSize, int outputSize) {
         Layer previousLayer = this->layers(layer-1);
         this->layers(layer).input = Eigen::VectorXd(previousLayer.output.size());
         this->layers(layer).output = Eigen::VectorXd(neuronsPerLayer);
+        this->layers(layer).output.setZero();
     }
     // ======================================================================================================
     
@@ -79,9 +86,32 @@ NN::NN(int layers, int neuronsPerLayer, int inputSize, int outputSize) {
         }
     }
     // ===========================================================================================================
+    
+    // ============= Initialize the output layer ================
+    outputLayer.neurons = Eigen::VectorX<Neuron>(outputSize);
+    
+    int lastLayerIndex = layers - 1;
+    Layer lastHiddenLayer = this->layers(lastLayerIndex);
+    outputLayer.input = lastHiddenLayer.output;
+    for (int neuron=0; neuron < outputSize; neuron++) {
+        outputLayer.neurons(neuron).weights = Eigen::VectorXd(outputLayer.input.size());
+        
+        for (auto &weight : outputLayer.neurons(neuron).weights) {
+            weight = WInit::xavierInit(inputSize, outputSize);
+        }
+    }
+    // =========================================================
 }
 
 // TODO
 void NN::forward() {
+    Layer &firstLayer = this->layers(0);
+    for (int neuron=0; neuron < firstLayer.neurons.size(); neuron++) {
+        double dotRes = firstLayer.neurons(neuron).weights.dot(firstLayer.input);
+        double output = Sigmoid(dotRes);
+        firstLayer.output(neuron) = output;
+    }
 
+    
+    std::cout << firstLayer.output << '\n';
 }
