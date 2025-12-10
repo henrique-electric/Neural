81f7689204eef35d2838680896f6efe231dfb4d3
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-12-10 16:17:58 -0300

    Adjust the Network structure

--- include/nn.hpp
+++ include/nn.hpp
@@ -1,32 +1,45 @@
 #pragma once
+
+#include <rng.hpp>
+#include <constants.hpp>
+#include <inits.hpp>
+
 #include <Eigen/Eigen>
 #include <cmath>
 #include <numbers>
 #include <iostream>
-#include <random>
 
-namespace Constants {
-    const double euler = 2.718281828459045f;
-    const double pi = 3.14159265358979f;
-}
+
 
 class NN
 {
-    struct Layer {
+    struct Neuron {
         double output;
-        Eigen::MatrixXd weights;
+        Eigen::VectorXd weights;
     };
     
+    struct Layer {
+        Eigen::VectorXd output;
+        Eigen::VectorXd input;
+        Eigen::VectorX<Neuron> neurons;
+    };
     
 private:
     Eigen::VectorXd inputs;
     Eigen::VectorX<Layer> layers;
+    Eigen::VectorX<Layer> output;
+
+    
+    void fillMatrix(Eigen::MatrixXd &matrix, int rows, int columns);
     
 public:
     static double Sigmoid(double x);
+    static double reLU(double x);
+    
     static void activation(Eigen::VectorXd &linearOutput);
+    static void ComputeLayer(Layer &layer, Eigen::VectorXd input);
     
     void forward();
 
-    NN(int layers, int neuronsPerLayer);
+    NN(int layers, int neuronsPerLayer, int inputSize, int outputSize);
 };
