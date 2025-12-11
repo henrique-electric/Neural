ee56aa5b3cd31498a3210bbf0f4aa8f3cf72243e
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-12-11 01:27:25 -0300

    Add some derivatives

--- include/loss.hpp
+++ include/loss.hpp
@@ -3,5 +3,11 @@
 
 namespace Loss {
     double SquareLoss(double result, double expected);
+    double MeanSquareLoss(Eigen::VectorXd &input, Eigen::VectorXd &expected);
     double CrossEntropy(Eigen::VectorXd &prediction, Eigen::VectorXd &trueLabel);
+    
+    double SigmoidDerivative(double x);
+    double WeightedSumDerivated(double x);
+    double SquareLossDerivative(double result, double expected);
+double MeanSquareLossDerivative(Eigen::VectorXd &input, Eigen::VectorXd &expected);
 }
