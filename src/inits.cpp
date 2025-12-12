2e1b7e9e1299002062a09b5e552284f8cacc0a7d
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-12-12 20:52:05 -0300

    Change the numerator to be a float

--- src/inits.cpp
+++ src/inits.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 double WInit::xavierInit(long numInputs, long numOutputs) {
-    double xavierRes = sqrt(6/(numInputs + numOutputs));
+    double xavierRes = sqrt(6.0/(numInputs + numOutputs));
     RNG::RNG rng(-xavierRes, xavierRes);
     
     return rng.rand();
