0f78494d4be5aa399b5402551f3c71a4427d09b2
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2026-04-14 11:24:55 -0300

    Move the COPILE_UTILS functions used for debbuging to a their own file.

--- src/nn.cpp
+++ src/nn.cpp
@@ -118,25 +118,3 @@ void NN::forward() {
 }
 
 
-#ifdef COMPILE_UTILS
-void NN::printLayerWeights(void) {
-    for (int layer=0; layer < this->layers.size(); layer++) {
-        std::cout << "Weights from layer " << layer << '\n';
-        std::cout << this->layers(layer).weights << "\n\n";
-    }
-}
-
-void NN::printLayerOutputs(void) {
-    for (int layer = 0; layer < this->layers.size(); layer++) {
-        std::cout << "Output from layer " << layer << '\n';
-        std::cout << this->layers(layer).output << "\n";
-    }
-}
-
-void NN::printLayerInputs(void) {
-    for (int layer = 0; layer < this->layers.size(); layer++) {
-        std::cout << "Input from layer " << layer << '\n';
-        std::cout << this->layers(layer).input << "\n";
-    }
-}
-#endif
